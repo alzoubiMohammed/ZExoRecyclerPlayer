d03bd564020f88ea86ecfa3847eaafa37d96ba7d
mohammed alzoubi <alzoubi.mohammed94@gmail.com>
2022-08-10 20:04:11 +0300

    add project

--- app/src/main/java/com/zoubi/exorecyclervideoplayer/MainActivity.kt
+++ app/src/main/java/com/zoubi/exorecyclervideoplayer/MainActivity.kt
@@ -3,12 +3,13 @@ package com.zoubi.exorecyclervideoplayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
+import com.zoubi.exorecyclervideoplayer.common.setupWithExoplayer
 
 class MainActivity : AppCompatActivity() {
     lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+//recyclerView.setupWithExoplayer()
     }
 }
